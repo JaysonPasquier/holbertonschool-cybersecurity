27c1b5c5189e34a97216b4d41256c58e5bfa7526
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2026-04-13 11:48:58 -0400

    third task

--- network_security/2x00_network_fundamentals/3-ip2bin.sh
+++ network_security/2x00_network_fundamentals/3-ip2bin.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$1" | awk -F. '{for(i=1;i<=4;i++){b="";n=$i;for(j=7;j>=0;j--){b=b int(n/2^j)%2};printf b (i<4?".":"\n")}}'
+IFS=. read -r a b c d <<<"$1"; printf "%08d.%08d.%08d.%08d\n" "$(echo "obase=2; $a" | bc)" "$(echo "obase=2; $b" | bc)" "$(echo "obase=2; $c" | bc)" "$(echo "obase=2; $d" | bc)"
