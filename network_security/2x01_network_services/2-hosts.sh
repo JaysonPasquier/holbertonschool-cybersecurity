56e2b20af12c07ccae3c356aaa9b239e7937194f
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2026-04-15 14:30:44 -0400

    third script fixed

--- network_security/2x01_network_services/2-hosts.sh
+++ network_security/2x01_network_services/2-hosts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '/(^| )localhost($| )/ { for(i=1; i<=NF; i++) if($i ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/){ print $i; exit } }' /etc/hosts
+awk '/(^|[[:space:]])localhost($|[[:space:]])/ {print $1; exit}' /etc/hosts
