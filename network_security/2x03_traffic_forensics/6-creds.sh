fd88dcae9d771b64eb4899489202054ff82d7c7a
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2026-04-27 10:29:15 -0400

    fixed

--- network_security/2x03_traffic_forensics/6-creds.sh
+++ network_security/2x03_traffic_forensics/6-creds.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tshark -r "$1" -T fields -e http.file_data | tr "&" "\n" | grep -E "^(password|pass|pwd)=" | cut -d"=" -f2
+tshark -r "$1" -T fields -e urlencoded-form.value | tr "&" "\n" | grep -E "^(password|pass|pwd)=" | cut -d"=" -f2
 
