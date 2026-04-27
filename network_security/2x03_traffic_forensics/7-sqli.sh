e4ead743e63ca2bde648cb824c12017238c5b6ef
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2026-04-27 11:15:30 -0400

    fixed

--- network_security/2x03_traffic_forensics/7-sqli.sh
+++ network_security/2x03_traffic_forensics/7-sqli.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tshark -r "$1" -T fields -e http.request.uri | grep -iE "(union|select|%20union|%20select|union%20|select%20)"
+tshark -r "$1" -Y 'http.request.uri contains "UNION" or http.request.uri contains "SELECT" or http.request.uri contains "union" or http.request.uri contains "select"' -T fields -e http.request.uri
 
