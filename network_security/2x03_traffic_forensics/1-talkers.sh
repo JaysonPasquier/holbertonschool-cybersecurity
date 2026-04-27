376cdca14c234be547e5cbeb147562e77859a915
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2026-04-27 09:37:15 -0400

    removed -q

--- network_security/2x03_traffic_forensics/1-talkers.sh
+++ network_security/2x03_traffic_forensics/1-talkers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tshark -r "$1" -T fields -e ip.src -q | sort | uniq -c | sort -rn | awk '{print $2}'
+tshark -r "$1" -T fields -e ip.src | sort | uniq -c | sort -rn | awk '{print $2}'
 
