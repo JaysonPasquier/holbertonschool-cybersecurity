4270722b5dfab498996315b0fca3547d492a1bb0
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2026-04-30 07:52:17 -0400

    add 403 check to test scripts

--- network_security/2x05_interceptor/5-test-blocked.sh
+++ network_security/2x05_interceptor/5-test-blocked.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-curl -x http://10.200.0.1:3128 -o /dev/null -s -w "%{http_code}" http://malware.com
+
+result=$(curl -x http://10.200.0.1:3128 -o /dev/null -s -w "%{http_code}" http://malware.com)
+echo "$result"
+[ "$result" = "403" ]
--- network_security/2x05_interceptor/7-test-extension.sh
+++ network_security/2x05_interceptor/7-test-extension.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-curl -x http://10.200.0.1:3128 -o /dev/null -s -w "%{http_code}" http://example.com/test.exe
+
+result=$(curl -x http://10.200.0.1:3128 -o /dev/null -s -w "%{http_code}" http://example.com/test.exe)
+echo "$result"
+[ "$result" = "403" ]
